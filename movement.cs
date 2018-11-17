b02a45ad399282eaafbfc37b8a729b5a274e5f9d
erikwilder9009 <43395743+erikwilder9009@users.noreply.github.com>
2018-11-17 00:59:53 -0500

    Add files via upload

---
+++ movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+    Transform transf;
+
+	// Use this for initialization
+	void Start () {
+        transf = gameObject.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            transf.Translate(0, 1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transf.Translate(-1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transf.Translate(0, -1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transf.Translate(1, 0, 0);
+        }
+
+
+    }
+}
