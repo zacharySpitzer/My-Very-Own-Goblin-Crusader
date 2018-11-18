b7bb26d07e296d02882dc7bb212d1fa559c7d635
erikwilder9009 <43395743+erikwilder9009@users.noreply.github.com>
2018-11-18 06:34:27 -0500

    Add files via upload

---
+++ scripts/mindlessAI.cs
@@ -0,0 +1,358 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Not turn based?
+
+public class mindlessAI : MonoBehaviour
+{
+    movement moveScript;
+
+    public bool isBoss;
+    public int knockback;
+
+    public int maxHP;
+    public int currentHP;
+
+    public LayerMask wall;
+    public LayerMask interactable;
+    public LayerMask player;
+
+    public bool chase;
+    public float chaseDistance;
+    
+    int count = 0;
+    public int moveWait;
+
+    // Smack a bitch
+    public Transform target;
+    public float attackRange;
+    public int meleeDamage;
+    private float lastAttackTime;
+    public float attackDelay;
+    
+
+    public bool copperAdd;
+    public bool silverAdd;
+    public bool goldAdd;
+    public bool platinumAdd;
+
+    //int acount;
+    //int bcount;
+    //int ccount;
+    //int dcount;
+    //int ecount;
+
+    private void Start()
+    {
+
+        moveScript = GetComponent<movement>();
+    }
+
+
+
+    public void EnemyTurn()
+    {
+        //Debug.Log("Enemy Turn");
+
+        int ran = Random.Range(0, 4);
+        count++;
+
+        float distanceToPlayer = Vector3.Distance(transform.position, target.position);
+
+        if (distanceToPlayer < attackRange)
+        {
+            if (Time.time > lastAttackTime + attackDelay)
+            {
+                Debug.Log(meleeDamage);
+                movement.instance.TakeDamage(meleeDamage, isBoss, knockback);
+
+                lastAttackTime = Time.time;
+            }
+        }
+
+
+        if (!isClose(chaseDistance) || (isClose(chaseDistance) && !chase))
+        {
+            count = 0;
+
+            if (isUpBlocked() == 0 && ran == 0)
+            {
+
+                gameObject.transform.Translate(0, 1f, 0);
+                //acount++;
+                //Debug.Log(ran);
+            }
+
+            if (isLeftBlocked() == 0 && ran == 2)
+            {
+
+                gameObject.transform.Translate(-1f, 0, 0);
+                //bcount++;
+                //Debug.Log(ran + " : " + bcount);
+            }
+            if (isDownBlocked() == 0 && ran == 1)
+            {
+
+                gameObject.transform.Translate(0, -1f, 0);
+                //ccount++;
+                //Debug.Log(ran + " : " + ccount);
+
+            }
+            if (isRightBlocked() == 0 && ran == 3)
+            {
+
+
+                gameObject.transform.Translate(1f, 0, 0);
+                //dcount++;
+                //Debug.Log(ran + " : " + dcount);
+            }
+            else if (ran == 2)
+            {
+                gameObject.transform.Translate(0, 0, 0);
+                //ecount++;
+                //Debug.Log(ran + " : " + ecount);
+            }
+        }
+        else if (isUpBlocked() != 3 && isDownBlocked() != 3 && isRightBlocked() != 3 && isLeftBlocked() != 3 && chase)
+        {
+            count = 0;
+            gameObject.transform.Translate(getMove());
+            Debug.Log(getMove());
+        }
+        else
+        {
+        }
+
+
+
+        //Debug.Log(ran);
+
+
+        //Debug.Log(getDirection());
+        //Debug.Log(getMove());
+        //Debug.Log("Close? " + isClose(chaseDistance));
+    }
+
+
+
+
+
+
+
+
+
+
+    // 0 = nothing 1 = wall 2 = interactable 3 = enemy
+
+    int isDownBlocked()
+    {
+        Vector2 position = transform.position;
+        Vector2 downdir = (Vector2.down) * 1f;
+        float distance = 1f;
+
+        Debug.DrawRay(position, downdir, Color.red);
+        RaycastHit2D block = Physics2D.Raycast(position, downdir, distance, wall);
+        RaycastHit2D inter = Physics2D.Raycast(position, downdir, distance, interactable);
+        RaycastHit2D p = Physics2D.Raycast(position, downdir, distance, player);
+
+        if (block)
+        {
+            //Debug.Log("1D");
+            return 1;
+        }
+        if (inter)
+        {
+            //Debug.Log(inter.collider.name);
+            return 2;
+        }
+        if (p)
+        {
+            Debug.Log("P");
+            return 3;
+        }
+
+        return 0;
+    }
+
+    int isUpBlocked()
+    {
+        Vector2 position = transform.position;
+        Vector2 updir = (Vector2.up) * 1f;
+        float distance = 1f;
+
+        Debug.DrawRay(position, updir, Color.red);
+        RaycastHit2D block = Physics2D.Raycast(position, updir, distance, wall);
+        RaycastHit2D inter = Physics2D.Raycast(position, updir, distance, interactable);
+        RaycastHit2D p = Physics2D.Raycast(position, updir, distance, player);
+
+        if (block)
+        {
+            //Debug.Log("1U");
+            return 1;
+        }
+        if (inter)
+        {
+            //Debug.Log(inter.collider.name);
+            return 2;
+        }
+        if (p)
+        {
+            Debug.Log("P");
+            return 3;
+        }
+
+        return 0;
+    }
+
+    int isLeftBlocked()
+    {
+        Vector2 position = transform.position;
+        Vector2 leftdir = (Vector2.left) * 1f;
+        float distance = 1f;
+
+        Debug.DrawRay(position, leftdir, Color.red);
+        RaycastHit2D block = Physics2D.Raycast(position, leftdir, distance, wall);
+        RaycastHit2D inter = Physics2D.Raycast(position, leftdir, distance, interactable);
+        RaycastHit2D p = Physics2D.Raycast(position, leftdir, distance, player);
+
+        if (block)
+        {
+            //Debug.Log("1L");
+            return 1;
+        }
+        if (inter)
+        {
+            //Debug.Log(inter.collider.name);
+            return 2;
+        }
+        if (p)
+        {
+            Debug.Log("P");
+            return 3;
+        }
+
+        return 0;
+    }
+
+    int isRightBlocked()
+    {
+        Vector2 position = transform.position;
+        Vector2 rightdir = (Vector2.right) * 1f;
+        float distance = 1f;
+
+        Debug.DrawRay(position, rightdir, Color.red);
+        RaycastHit2D block = Physics2D.Raycast(position, rightdir, distance, wall);
+        RaycastHit2D inter = Physics2D.Raycast(position, rightdir, distance, interactable);
+        RaycastHit2D p = Physics2D.Raycast(position, rightdir, distance, player);
+
+        if (block)
+        {
+            //Debug.Log("1R");
+            return 1;
+        }
+        if (inter)
+        {
+            //Debug.Log(inter.collider.name);
+            return 2;
+        }
+        if (p)
+        {
+            Debug.Log("P");
+            return 3;
+        }
+
+        return 0;
+    }
+
+
+
+
+    public void takeDamage(int damage)
+    {
+        currentHP -= damage;
+
+        if (currentHP <= 0)
+        {
+            
+            if (copperAdd)
+            {
+                movement.instance.copperAdd();
+            }
+            if (silverAdd)
+            {
+                movement.instance.silverAdd();
+            }
+            if (goldAdd)
+            {
+                movement.instance.goldAdd();
+            }
+            if (platinumAdd)
+            {
+                movement.instance.platinumAdd();
+            }
+            Destroy(gameObject);
+        }
+    }
+
+    Vector3 getDirection()
+    {
+        return (gameObject.transform.position - movement.instance.playerLoc());
+    }
+    bool isClose(float chaseDistance)
+    {
+        if ((getDirection().x < chaseDistance && getDirection().x > -chaseDistance)
+            && (getDirection().y < chaseDistance && getDirection().y > -chaseDistance))
+        {
+            return true;
+        }
+        if ((getDirection().x == 1 || getDirection().x == -1)
+            && (getDirection().y == 1 || getDirection().y == -1))
+        {
+            return false;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    Vector3 getMove()
+    {
+        Vector3 mov = Vector3.zero;
+
+        if (getDirection().y < 0 && isUpBlocked() == 0)
+        {
+            mov.y = 1;
+            return mov;
+        }
+        if (getDirection().y > 0 && isDownBlocked() == 0)
+        {
+            mov.y = -1;
+            return mov;
+        }
+
+        if (getDirection().x < 0 && isRightBlocked() == 0)
+        {
+            mov.x = 1;
+            return mov;
+        }
+        if (getDirection().x > 0 && isLeftBlocked() == 0)
+        {
+            mov.x = -1;
+            return mov;
+        }
+
+        return Vector3.zero;
+
+    }
+
+
+
+
+
+
+
+
+
+}
---
+++ scripts/movement.cs
@@ -0,0 +1,438 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public static movement instance;
+
+    public int turnpause = 0;
+    int count = 0;
+
+    public int maxHP = 50;
+    public int currentHP;
+    public int gold = 0;
+
+    // Gold Types
+    int copperCoin = 100;
+    int silverCoin = 1000;
+    int goldCoin = 10000;
+    int platinumCoin = 1000000;
+
+    public GameObject front;
+    public GameObject leftSide;
+    public GameObject rightSide;
+    public GameObject back;
+
+    Transform transf;
+    public LayerMask wall;
+    public LayerMask interactable;
+    public LayerMask enemy;
+
+    public GameObject axRot;
+    public int axeStrength = 1;
+    Damageable damageableComponent;
+    public int pickupheal = 10;
+
+    public List<GameObject> enemys;
+
+    mindlessAI targ;
+    chest ctarg;
+    hpickup htarg;
+
+    // Use this for initialization
+    void Start()
+    {
+        instance = this;
+        transf = gameObject.GetComponent<Transform>();
+        currentHP = maxHP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("V : " + Input.GetAxis("Vertical"));
+        Debug.Log("H : " + Input.GetAxis("Horizontal"));
+        count++;
+        if (count >= turnpause)
+        {
+            if (isUpBlocked() == 0 && Input.GetAxis("Vertical") == 1)
+            {
+                count = 0;
+                transf.Translate(0, 1f, 0);
+                front.SetActive(false);
+                leftSide.SetActive(false);
+                rightSide.SetActive(false);
+                back.SetActive(true);
+                foreach (GameObject go in enemys)
+                {
+                    mindlessAI baddy = go.gameObject.GetComponent<mindlessAI>();
+                    if (baddy)
+                    {
+                        baddy.EnemyTurn();
+                    }
+                }
+            }
+
+            if (isLeftBlocked() == 0 && Input.GetAxis("Horizontal") == -1)
+            {
+                count = 0;
+                transf.Translate(-1f, 0, 0);
+                front.SetActive(false);
+                leftSide.SetActive(true);
+                rightSide.SetActive(false);
+                back.SetActive(false);
+                foreach (GameObject go in enemys)
+                {
+                    mindlessAI baddy = go.gameObject.GetComponent<mindlessAI>();
+                    if (baddy)
+                    {
+                        baddy.EnemyTurn();
+                    }
+                    else
+                    {
+                        count = 0;
+                        return;
+                    }
+                }
+            }
+
+            if (isDownBlocked() == 0 && Input.GetAxis("Vertical") == -1)
+            {
+                count = 0;
+                transf.Translate(0, -1f, 0);
+                front.SetActive(true);
+                leftSide.SetActive(false);
+                rightSide.SetActive(false);
+                back.SetActive(false);
+                foreach (GameObject go in enemys)
+                {
+                    mindlessAI baddy = go.gameObject.GetComponent<mindlessAI>();
+                    if (baddy)
+                    {
+                        baddy.EnemyTurn();
+                    }
+                }
+            }
+
+            if (isRightBlocked() == 0 && Input.GetAxis("Horizontal") == 1)
+            {
+                count = 0;
+                transf.Translate(1f, 0, 0);
+                front.SetActive(false);
+                leftSide.SetActive(false);
+                rightSide.SetActive(true);
+                back.SetActive(false);
+                foreach (GameObject go in enemys)
+                {
+                    mindlessAI baddy = go.gameObject.GetComponent<mindlessAI>();
+                    if (baddy)
+                    {
+                        baddy.EnemyTurn();
+                    }
+                }
+            }
+
+            if (Input.GetButtonDown("SkipTurn"))
+            {
+                count = 0;
+                foreach (GameObject go in enemys)
+                {
+                    mindlessAI baddy = go.gameObject.GetComponent<mindlessAI>();
+                    if (baddy)
+                    {
+                        baddy.EnemyTurn();
+                    }
+                }
+            }
+            if ((isRightBlocked() == 2 || isUpBlocked() == 2 || isDownBlocked() == 2 || isLeftBlocked() == 2) && Input.GetButtonDown("Interact"))
+            {
+                count = 0;
+                if (ctarg)
+                {
+                    ctarg.Open();
+                }
+                if (htarg)
+                {
+                    Heal();
+                    Destroy(htarg.gameObject);
+                }
+
+                foreach (GameObject go in enemys)
+                {
+                    mindlessAI baddy = go.gameObject.GetComponent<mindlessAI>();
+                    if (baddy)
+                    {
+                        baddy.EnemyTurn();
+                    }
+                    else
+                    {
+                        count = 0;
+                        return;
+                    }
+                }
+            }
+
+
+            if (Input.GetButtonDown("Atack"))
+            {
+                count = 0;
+                axRot.SetActive(true);
+                axRot.transform.Rotate(0, 0, -3);
+                if ((isRightBlocked() == 3 || isUpBlocked() == 3 || isDownBlocked() == 3 || isLeftBlocked() == 3))
+                {
+                    targ.takeDamage(axeStrength);
+                    Debug.Log("Hit");
+                }
+
+                foreach (GameObject go in enemys)
+                {
+                    mindlessAI baddy = go.gameObject.GetComponent<mindlessAI>();
+                    if (baddy)
+                    {
+                        baddy.EnemyTurn();
+                    }
+                    else
+                    {
+                        count = 0;
+                        return;
+                    }
+                }
+            }
+            else
+            {
+
+                axRot.SetActive(false);
+                axRot.transform.rotation = Quaternion.Euler(0, 0, 15);
+            }
+
+            LoadScript.instance.Wealth(gold);
+            LoadScript.instance.Health(currentHP);
+
+            count = 0;
+        }
+        // THIS IS WHAT WE SHOULD DO FOR MOVEMENT WITH GETAXIS
+
+        //transf.Transltate(Input.GetAxis("Horizontal"), Input.GetAxis("Horizontal"), 0);
+
+        // Than set inputs to snapto, so they are always 1, 0, or -1. This will also
+        // add horizontal movement in our final build.
+
+        // all movement in one line.
+
+
+        
+    }
+
+    public void copperAdd()
+    {
+        gold += copperCoin;
+    }
+
+    public void silverAdd()
+    {
+        gold += silverCoin;
+    }
+
+    public void goldAdd()
+    {
+        gold += goldCoin;
+    }
+
+    public void platinumAdd()
+    {
+        gold += platinumCoin;
+    }
+
+    // 0 = nothing 1 = wall 2 = interactable 3 = enemy
+
+    int isDownBlocked()
+    {
+        Vector2 position = transform.position;
+        Vector2 downdir = (Vector2.down) * 1f;
+        float distance = 1f;
+
+        Debug.DrawRay(position, downdir, Color.red);
+        RaycastHit2D block = Physics2D.Raycast(position, downdir, distance, wall);
+        RaycastHit2D inter = Physics2D.Raycast(position, downdir, distance, interactable);
+        RaycastHit2D baddy = Physics2D.Raycast(position, downdir, distance, enemy);
+
+        if (block)
+        {
+            //Debug.Log("1D");
+            return 1;
+        }
+        if (inter)
+        {
+            //Debug.Log(inter.collider.name);
+            ctarg = inter.collider.GetComponent<chest>();
+            htarg = inter.collider.GetComponent<hpickup>();
+            return 2;
+        }
+        if (baddy)
+        {
+            targ = baddy.collider.GetComponent<mindlessAI>();
+            return 3;
+        }
+
+        return 0;
+    }
+
+    int isUpBlocked()
+    {
+        Vector2 position = transform.position;
+        Vector2 updir = (Vector2.up) * 1f;
+        float distance = 1f;
+
+        Debug.DrawRay(position, updir, Color.red);
+        RaycastHit2D block = Physics2D.Raycast(position, updir, distance, wall);
+        RaycastHit2D inter = Physics2D.Raycast(position, updir, distance, interactable);
+        RaycastHit2D baddy = Physics2D.Raycast(position, updir, distance, enemy);
+
+        if (block)
+        {
+            //Debug.Log("1U");
+            return 1;
+        }
+        if (inter)
+        {
+            //Debug.Log(inter.collider.name);
+            ctarg = inter.collider.GetComponent<chest>();
+            htarg = inter.collider.GetComponent<hpickup>();
+            return 2;
+        }
+        if (baddy)
+        {
+            targ = baddy.collider.GetComponent<mindlessAI>();
+            return 3;
+        }
+
+        return 0;
+    }
+
+    int isLeftBlocked()
+    {
+        Vector2 position = transform.position;
+        Vector2 leftdir = (Vector2.left) * 1f;
+        float distance = 1f;
+
+        Debug.DrawRay(position, leftdir, Color.red);
+        RaycastHit2D block = Physics2D.Raycast(position, leftdir, distance, wall);
+        RaycastHit2D inter = Physics2D.Raycast(position, leftdir, distance, interactable);
+        RaycastHit2D baddy = Physics2D.Raycast(position, leftdir, distance, enemy);
+
+        if (block)
+        {
+            //Debug.Log("1L");
+            return 1;
+        }
+        if (inter)
+        {
+            //Debug.Log(inter.collider.name);
+            ctarg = inter.collider.GetComponent<chest>();
+            htarg = inter.collider.GetComponent<hpickup>();
+            return 2;
+        }
+        if (baddy)
+        {
+            targ = baddy.collider.GetComponent<mindlessAI>();
+            return 3;
+        }
+
+        return 0;
+    }
+
+    int isRightBlocked()
+    {
+        Vector2 position = transform.position;
+        Vector2 rightdir = (Vector2.right) * 1f;
+        float distance = 1f;
+
+        Debug.DrawRay(position, rightdir, Color.red);
+        RaycastHit2D block = Physics2D.Raycast(position, rightdir, distance, wall);
+        RaycastHit2D inter = Physics2D.Raycast(position, rightdir, distance, interactable);
+        RaycastHit2D baddy = Physics2D.Raycast(position, rightdir, distance, enemy);
+
+        if (block)
+        {
+            //Debug.Log("1R");
+            return 1;
+        }
+        if (inter)
+        {
+            //Debug.Log(inter.collider.name);
+            ctarg = inter.collider.GetComponent<chest>();
+            htarg = inter.collider.GetComponent<hpickup>();
+            return 2;
+        }
+        if (baddy)
+        {
+            targ = baddy.collider.GetComponent<mindlessAI>();
+            return 3;
+        }
+
+        return 0;
+    }
+
+    public Vector3 playerLoc()
+    {
+        return transf.position;
+    }
+
+    public int Treasure()
+    {
+        return gold;
+    }
+
+    public void TakeDamage(int damage, bool isBoss, int knockback)
+    {
+        currentHP -= damage;
+        Debug.Log(damage);
+        if(isBoss)
+        {
+            Debug.Log("Boss Hit");
+            if (isRightBlocked() == 3)
+            {
+                transf.Translate(knockback, 0, 0);
+            }
+            if (isUpBlocked() == 3)
+            {
+                transf.Translate(0, knockback, 0);
+            }
+            if (isDownBlocked() == 3)
+            {
+                transf.Translate(0, -knockback, 0);
+            }
+            if (isLeftBlocked() == 3)
+            {
+                transf.Translate(-knockback, 0, 0);
+            }
+        }
+        
+        if (currentHP <= 0)
+        {
+            Debug.Log("Dead!");
+        }
+    }
+
+
+    public void Heal()
+    {
+        int chp = currentHP;
+        while (currentHP < (chp + pickupheal) && currentHP < maxHP)
+        {
+            currentHP++;
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        warp pad = other.gameObject.GetComponent<warp>();
+        if (pad)
+        {
+            Debug.Log("Town");
+            LoadScript.instance.LoadTown();
+        }
+    }
+}
