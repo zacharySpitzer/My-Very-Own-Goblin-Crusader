fb28b3da2fd6355ec4e521d0e8b6c9e572a883ea
zacharySpitzer <45111466+zacharySpitzer@users.noreply.github.com>
2018-11-18 16:13:33 -0500

    Add files via upload

---
+++ gameMusic.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameMusic : MonoBehaviour
+{
+    public GameObject MusicGameObject;
+   
+    AudioSource source;
+    AudioSource musicSource;
+    // Use this for initialization
+    void Start()
+    {
+        source = gameObject.AddComponent<AudioSource>();
+        musicSource = MusicGameObject.GetComponent<AudioSource>();
+    }
+   
+    public void ToggleSpacialBlend()
+    {
+        if (musicSource.spatialBlend == 1)
+        {
+            musicSource.spatialBlend = 0;
+            Debug.Log("Spatical Blend = 0 (2d)");
+        }
+        else
+        {
+            musicSource.spatialBlend = 1;
+            Debug.Log("Spatical Blend = 1 (3d)");
+        }
+    }
+}
