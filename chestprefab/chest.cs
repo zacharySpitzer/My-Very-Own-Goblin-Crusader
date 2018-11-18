aac3be13992ad6bedc26c58302fdb6cbdc8f1571
erikwilder9009 <43395743+erikwilder9009@users.noreply.github.com>
2018-11-18 00:00:29 -0500

    animated chest prefab

---
+++ chestprefab/chest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chest : MonoBehaviour {
+
+    public static chest instance;
+
+    public GameObject open;
+    public GameObject close;
+    public GameObject coin;
+
+	// Use this for initialization
+	void Start () {
+        instance = this;
+
+        close.SetActive(true);
+        open.SetActive(false);
+        coin.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void Open()
+    {
+        close.SetActive(false);
+        open.SetActive(true);
+        coin.SetActive(true);
+        Destroy(coin, 1);
+
+        movement.instance.silverAdd();
+
+        Debug.Log("Open");
+    }
+}
